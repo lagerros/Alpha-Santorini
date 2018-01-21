06eddafb85dd9dea50eeec189306a2738f85eb4b
lagerros <jacobklagerros@gmail.com>
2018-01-21 00:14:53 +0000

    Fixed batch-norm training

--- nets.py
+++ nets.py
@@ -22,8 +22,10 @@ def filter(in_channel, out_channel, kernel_size):
         
 def conv_block(features, f, activation_fn, phase, k):
     conv = tf.layers.conv2d(inputs=features, filters=f, kernel_size=[k, k], strides=[1, 1], padding="SAME")
-    return tf.contrib.layers.batch_norm(conv, center=True, scale=True, is_training=phase, 
-                                           updates_collections=None, activation_fn=activation_fn)
+    norm_conv = tf.layers.batch_normalization(conv, training=phase)
+    if activation_fn != None:
+        norm_conv = activation_fn(norm_conv)
+    return norm_conv
     
 def res_block(features, in_channel, out_channel, kernel_size, phase):
     conv1 = conv_block(features, 256, tf.nn.relu, phase, kernel_size)
@@ -37,6 +39,7 @@ def res_block(features, in_channel, out_channel, kernel_size, phase):
 class deep_net():
     # initial conv layer
     def __init__(self, phase, scope, lr):
+        self.name = scope
         with tf.variable_scope(scope):
             self.lr = lr
             self.phase = phase #whether training or not, for batch norm
@@ -44,7 +47,7 @@ class deep_net():
             self.tower0 = conv_block(self.s_t_in, 256, tf.nn.relu, self.phase, 3)
             
             # residual tower
-            height = 9
+            height = 12
             for x in range(height):
                previous = self.get_tower(x)
                setattr(self, 'tower'+str(x+1), res_block(previous, 256, 256, 3, self.phase))
@@ -60,6 +63,7 @@ class deep_net():
             #value head
             pre_v = conv_block(self.get_tower(height), 1, tf.nn.relu, self.phase, 1)
             self.pre_v = tf.contrib.layers.flatten(pre_v)
+            
             self.v = tf.contrib.layers.fully_connected(self.pre_v, num_outputs=1, activation_fn=tf.nn.tanh) 
     
             #reward signal and update
@@ -68,8 +72,13 @@ class deep_net():
             self.vars = tf.trainable_variables() 
             L2 = tf.add_n([tf.nn.l2_loss(w) for w in self.vars])
             self.loss = tf.transpose(tf.squared_difference(self.z,self.v)) - tf.nn.softmax_cross_entropy_with_logits(labels=self.pi,logits=self.p) + c*L2
-            self.optimizer = tf.train.MomentumOptimizer(lr, momentum).minimize(self.loss)
-       
+            
+            #We must manually make sure that batch norm updates the moving 
+            #averages and variances over time
+            update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS, scope=scope)
+            with tf.control_dependencies(update_ops):
+                self.optimizer = tf.train.MomentumOptimizer(lr, momentum).minimize(self.loss)
+      
     def get_tower(self, height):
         return getattr(self, 'tower'+str(height))
          
@@ -83,7 +92,7 @@ class deep_net():
     
     def P_and_v(self, s, legal_moves, sess):
         feed_dict = {self.s_t_in:s, self.legal_moves:legal_moves}
-        return sess.run([self.p, self.v], feed_dict=feed_dict)
+        return sess.run([self.p, self.v], feed_dict=feed_dict) 
         
     def train(self, s, z, pi, legal_moves, sess):
         self.phase = True
