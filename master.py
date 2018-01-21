6cc297767bde8a2f554a86b2823a654a3a28217b
lagerros <jacobklagerros@gmail.com>
2018-01-21 00:17:56 +0000

    Update master.py

--- master.py
+++ master.py
@@ -16,17 +16,16 @@ from time import sleep
 # OVERALL SETTINGS #
 ####################
 board_size = 5
-search_depth = int(input("Search depth?"))
+search_depth = int(input("Search depth?\n"))
 testing = 1
 training = 1
 v_resign = 0.75
 
 observe_games = 0
 explore = True
-load_model = bool(int(input("Load model? 0/1")))
-parallell = 1
-
-save_dir = "/home/jakey/"
+load_model = bool(int(input("Load model? 0/1\n")))
+parallell = int(input("Asynchronous? 0/1\n"))
+save_dir = input("Directory for saving weights:\n") 
 
 ####################
 # HELPER FUNCTIONS #
@@ -80,136 +79,165 @@ class train_set():
     
 coord = tf.train.Coordinator()
 
-def coordinator(f):
-    def wrapper(*args, evaluation=False, first_step=False):
-        i = 0
-        while not coord.should_stop():
-        # Keeps training/self-playing indefinitely until the evaluation is completed
-            if not evaluation: print("self-play game: %s" % i)
-            z = f(*args)
+
+class coordinator():
+    def __init__(self, condition):
+        # We only want to use the coordinator is the model is run in parallell
+        self.condition = condition
+        
+    def __call__(self, f):
+        if self.condition:
+            def wrapper(*args, evaluation=False, first_step=False):
+                i = 0
+                while not coord.should_stop():
+                # Keeps training/self-playing indefinitely until the evaluation is completed
+                    z = f(*args)
+                    if evaluation:
+                        return z
+                    else:
+                        print("Just finished self-play game: %s" % i)
+                        i += 1
+                        if first_step and i == 10:
+                            coord.request_stop()
+                print("Stop requested!")
+            return wrapper
+        return f
+P1 = None
+P2 = None
+
+@coordinator( parallell )
+def self_play(storage, player1, player2=None, explore=True, num_games=1):
+    for n in range(num_games):
+        if not parallell:
+            print("Self-play game: %s" % n)
+        game = santorini.Game() 
+    
+        p1 = M.MCTS(game, player1, sess, explore) 
+        
+        if player2 != None:
+            evaluation = True
+            p2 = M.MCTS(game, player2, sess, explore) 
+            players = [p1, p2]
+            global P1
+            P1 = p1
+            global P2
+            P2 = p2
+        else:
+            evaluation = False
+                   
+        temp_history = []
+    
+        done = False
+    
+        while done == False:
             if evaluation:
-                return z
+                player = game.turn_count%2    
+                tree = players[player]
+                other_tree = players[(player+1)%2]
             else:
-                i += 1
-                if first_step and i == 2:
-                    coord.request_stop()
-        print("Stop requested!")
-    return wrapper
+                tree = p1
+                
+            t0 = time.time()
+            done = tree.consider_resigning(v_resign, observe_games)  
+            a, pi_s, P = tree.run_simulation(search_depth)  #here's a difference
+            temp_history.extend([[game.stack_s(), pi_s, game.legal_moves(binaryV=True)]])
+            if evaluation:
+                other_tree.prepare_adversarial_move(a)
+            game.move(a)
+            done = game.done
+            if evaluation:
+                other_tree.finish_adversarial_move(a)
+            tree.prepare_next_move()
+    
+            if observe_games:
+                for i in range(10):
+                    print("\n")
+                print("P (predicted tree search probs):\n%s\n\n" % np.reshape(P, [5,5]),
+                      "pi (actual tree search probs):\n%s\n\n" % np.reshape(pi_s, [5,5]),
+                      "Chosen move: %s\n" % a,
+                      "Overall game state:\n%s\n\n" % game.render())
+                print("time: ", time.time()-t0)
+        z = game.outcome
+        
+        #store data
+        for entry in temp_history:
+            storage.add(entry[0], entry[1], z, entry[2])
+        
+    return z
+  
+@coordinator( parallell )
+def train(history, network, epochs=1):
+    print("Training...")
+    for epoch in range(epochs):    
+        try:
+            s0, pi_s0, z0, legal_moves0 = history.sample(500)
+        except:
+            try:
+                s0, pi_s0, z0, legal_moves0 = history.sample(100)
+            except:
+                print("no training, history is too empty")
+                if parallell:
+                    sleep(60)
+                return None
+    
+        losses = []
+        for t in range(8): # MVP of exploiting eight possible symmetries
+            s, z, pi_s, legal_moves = r(s0, z0, pi_s0, legal_moves0)
+            loss = network.train(s, z, pi_s, legal_moves, sess)
+            losses.append(loss)
+            
+        print("loss: ", np.mean(losses))  
 
 
-def evaluator(storage, challenger, champion, coord, sess, worker_threads):
+
+def evaluator(storage, challenger, champion, coord, sess, num_games=10):
     current_streak = 0
     players = [challenger, champion]
     outcomes = [-1, 1]
-    for i in range(5):
-        print("Evaluation game: %s" % i)
+    for i in range(num_games):
         #Randomize who goes first
         coinflip = np.random.choice(2)
         player1 = players[coinflip]
         player2 = players[(coinflip+1)%2]
-        z = self_play(storage, player1, player2, False, evaluation=True)
-
+        if parallell:
+            z = self_play(storage, player1, player2, False, evaluation=True)
+        else:
+            z = self_play(storage, player1, player2, False)
+            
         if z == outcomes[coinflip]:
             current_streak += 1         
-                                         
-        if current_streak >= 3:
+        print("Challenger's current streak %s" % current_streak)
+        if current_streak >= 0.6*num_games:
             print("We have a new champion! Updating weights...")
             # Tell self-play and optimization to stop in order to be restarted 
             # with the new champion
-            coord.request_stop()
-            # Crown the new champion by copying its weights to the past champion
-            sess.run(update_target_graph("Test_challenger", "Champion"))
-            # Make room for the new contender
-            sess.run(update_target_graph("Challenger", "Test_challenger"))
+            if parallell:
+                coord.request_stop()
+                # Crown the new champion by copying its weights to the past champion
+                sess.run(update_target_graph("Test_challenger", "Champion"))
+                # Make room for the new contender
+                sess.run(update_target_graph("Challenger", "Test_challenger"))
+            else:
+                sess.run(update_target_graph("Challenger", "Champion"))
             break
-        if 4-i+current_streak < 3:
+        if num_games-1-i+current_streak < 0.6*num_games:
             break
         
     #reset the challenger in case the new weights didn't beat the champion
-    if current_streak < 6:
+    if current_streak < 0.6*num_games:
         print("Challenger couldn't meet the challenge... resetting weights to previous champion...")
-        #Request that the other threads stop and wait for them to do so
-        coord.request_stop()
-        #Copy the champion to train to a new challenger
-        sess.run(update_target_graph("Champion", "Challenger"))
-        #Make room for the waiting challenger
-        sess.run(update_target_graph("Challenger", "Test_challenger"))
-        
- 
-@coordinator
-def self_play(storage, player1, player2=None, explore=True):
-    game = santorini.Game() 
-
-    p1 = M.MCTS(game, player1, sess, explore) 
-
-    if player2 != None:
-        evaluation = True
-        p2 = M.MCTS(game, player2, sess, explore) 
-        players = [p1, p2]
-    else:
-        evaluation = False
-               
-    temp_history = []
-
-    done = False
-
-    while done == False:
-        if evaluation:
-            player = game.turn_count%2    
-            tree = players[player]
-            other_tree = players[(player+1)%2]
+        if parallell:
+            #Request that the other threads stop and wait for them to do so
+            coord.request_stop()
+            #Copy the champion to train to a new challenger
+            sess.run(update_target_graph("Champion", "Challenger"))
+            #Make room for the waiting challenger
+            sess.run(update_target_graph("Challenger", "Test_challenger"))
         else:
-            tree = p1
+            sess.run(update_target_graph("Champion", "Challenger"))
             
-        t0 = time.time()
-        done = tree.consider_resigning(v_resign, observe_games)  
-        a, pi_s, P = tree.run_simulation(search_depth)
-        temp_history.extend([[game.stack_s(), pi_s, game.legal_moves(binaryV=True)]])
-        if evaluation:
-            other_tree.prepare_adversarial_move(a)
-        game.move(a)
-        done = game.done
-        if evaluation:
-            other_tree.finish_adversarial_move(a)
-        tree.prepare_next_move()
-
-        if observe_games:
-            for i in range(10):
-                print("\n")
-            print("P (predicted tree search probs):\n%s\n\n" % np.reshape(P, [5,5]),
-                  "pi (actual tree search probs):\n%s\n\n" % np.reshape(pi_s, [5,5]),
-                  "Chosen move: %s\n" % a,
-                  "Overall game state:\n%s\n\n" % game.render())
-            print("time: ", time.time()-t0)
-    z = game.outcome
-    
-    #store data
-    for entry in temp_history:
-        storage.add(entry[0], entry[1], z, entry[2])
-        
-    return z
-     
-@coordinator
-def train(history, network):    
-    print("Training...")
-    try:
-        s0, pi_s0, z0, legal_moves0 = history.sample(500)
-    except:
-        try:
-            s0, pi_s0, z0, legal_moves0 = history.sample(100)
-        except:
-            print("no training, history is too empty")
-            sleep(60)
-            return None
-
-    losses = []
-    for t in range(8): # MVP of exploiting eight possible symmetries
-        s, z, pi_s, legal_moves = r(s0, z0, pi_s0, legal_moves0)
-        loss = network.train(s, z, pi_s, legal_moves, sess)
-        losses.append(loss)
-        
-    print("loss: ", np.mean(losses))  
-  
+            
+ 
 history = train_set()
 
 game_history = []  #can do lots with this in order to store and generate games easily!
@@ -239,37 +267,52 @@ with tf.Session() as sess:
         update_target_graph("Champion", "Challenger")
         update_target_graph("Champion", "Test_challenger")
           
-    print("Starting self-play...")
     sess.run(tf.global_variables_initializer())
     
-    if not(parallell):
-        self_play(history, challenger)
-    else:
-        
-        num_workers = multiprocessing.cpu_count()
-        print("Utilizing %s CPUs" % num_workers)
-        
-        
-        def create_thread(worker):
-            t = threading.Thread(target=(worker))
-            t.start()
-            global worker_threads
-            worker_threads.append(t)
-            sleep(0.1)            
-                
-        for step in range(steps):
-            print("Step: ", step)
+    for step in range(steps):
+        print("Step: ", step)
 
-            worker_threads = [] 
-            if step == 0: first_step = True
-            else: first_step = False
-            print(first_step)
-            for i in range(num_workers-int(not(first_step))):
-                create_thread( lambda: self_play(history, champion, first_step=first_step) )
-            create_thread( lambda: train(history, challenger))
-            if step > 0:
-                create_thread( lambda: evaluator(history, test_challenger, champion, coord, sess, worker_threads) )
-            coord.join(worker_threads) #maybe put this inside evaluator?
-            print("Saving current champion...")
-            saver.save(sess, save_dir+"champion.ckpt")
-            coord.clear_stop()
+        if not(parallell):
+           
+           
+           print("Starting self-play...")
+           self_play(history, challenger, num_games=2)
+           
+           train(history, challenger, epochs=2)
+           
+           print("Evaluating new challenger...")
+           evaluator(history, challenger, champion, coord, sess, num_games=3)
+
+           
+           print("Saving...")
+           saver.save(sess, save_dir+"champion.ckpt")
+            
+        else:
+            num_workers = multiprocessing.cpu_count()
+            print("Utilizing %s CPUs" % num_workers)
+            
+            def create_thread(worker):
+                t = threading.Thread(target=(worker))
+                t.start()
+                global worker_threads
+                worker_threads.append(t)
+                sleep(0.1)            
+                    
+                
+            # Currently an issue here, as it gets stuck training and never proceeds beyond step 0
+            # maybe train just has to receive coord as input?
+    
+                worker_threads = [] 
+                if step == 0: first_step = True
+                else: first_step = False
+                print(first_step)
+                for i in range(num_workers-int(not(first_step))):
+                    create_thread( lambda: self_play(history, champion, first_step=first_step) )
+                create_thread( lambda: train(history, challenger))
+                if step > 0:
+                    create_thread( lambda: evaluator(history, test_challenger, champion, coord, sess, worker_threads) )
+                coord.join(worker_threads) #maybe put this inside evaluator?
+                print("Saving current champion...")
+                saver.save(sess, save_dir+"champion.ckpt")
+                coord.clear_stop()
+    
